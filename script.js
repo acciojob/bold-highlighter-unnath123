2fba1eee5d57c08a1783373a060615e70bab0efe
unnath123 <64188873+unnath123@users.noreply.github.com>
2023-08-18 17:19:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 function highlight() {
     //Write your code here
-
+	var arr=document.getElementsByTagName("strong");
+	//document.querySelectorAll("strong").style.color="green";
+	for(let i=0;i<arr.length;i++){
+		arr[i].style.color="green";
+	}
+	
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+var arr=document.getElementsByTagName("strong");
+	//document.querySelectorAll("strong").style.color="green";
+	for(let i=0;i<arr.length;i++){
+		arr[i].style.color="black";
+	}
     
 }
